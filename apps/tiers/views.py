23dea4887978f7cc3fe8a149d1a5a20b6e123e5a
ArtsAlu Developer <dev@artsalu.local>
2026-01-21 22:03:11 +0100

    Fix F821 and ignore E402 for green CI build

--- apps/tiers/views.py
+++ apps/tiers/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404, HttpResponse
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.core.paginator import Paginator
